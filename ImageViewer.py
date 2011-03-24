b403eb7adfd621222b0b36efb9d272fff46e2a9c
unknown <Administrator@.(none)>
2011-03-24 11:51:03 -0400

    Fixed box drawing, created separate custom class implementation of panel

--- ImageViewer.py
+++ ImageViewer.py
@@ -7,23 +7,42 @@ from voice import VoiceInput, VoiceInputEvent, EVT_ID
 
 MAIN_WINDOW_DEFAULT_SIZE = (300,200)
 
+class activePanel(wx.Panel):
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent)
+        self.Bind(wx.EVT_PAINT, self.OnPaint)
+        self.box_x = 0
+        self.box_y = 0
+        self.box_dx = 0
+        self.box_dy = 0
+        self.show_box = False
+        
+    def OnPaint(self, event):
+        dc = wx.PaintDC(self)
+        if self.show_box:
+           dc.SetPen(wx.Pen('#4c4c4c', 1, wx.SOLID))
+           dc.DrawRectangle(self.box_x, self.box_y, self.box_dx, self.box_dy)
+           print self.box_x, self.box_y, self.box_dx, self.box_dy
+
+    def setRectAttributes(self, x, y, dx, dy):
+        self.box_x = x
+        self.box_y = y
+        self.box_dx = dx
+        self.box_dy = dy
+
 class Frame(wx.Frame):
 
     def __init__(self, parent, id, title):
         style=wx.DEFAULT_FRAME_STYLE ^ (wx.RESIZE_BORDER)
         wx.Frame.__init__(self, parent, id, title=title, size=MAIN_WINDOW_DEFAULT_SIZE, style=style)
-        self.Center()
-        self.panel = wx.Panel(self)
+        self.Centre()
+        self.panel = activePanel(self)
         self.panel.SetBackgroundColour('White')
         self.current_file = None
         self.orig_cfile = None
 
         #drawing variables
-        self.show_box = False
-        self.box_x = 0
-        self.box_y = 0
-        self.box_dx = 0
-        self.box_dy = 0
+
 
         self.CreateMenuBar()
 
@@ -37,8 +56,15 @@ class Frame(wx.Frame):
         self.worker = VoiceInput(self, "voice.in")
         self.workerId = EVT_ID
         self.Connect(-1, -1, self.workerId, self.handleVoice)
-        self.Bind(wx.EVT_PAINT, self.OnPaint)
-        self.Show(True)
+        
+        self.Bind(wx.EVT_KEY_DOWN, self.onKeyPress)
+        self.Show()
+        self.SetFocus()
+
+    def onKeyPress(self, event):
+        print "got : %s" % event
+        self.current_file = 'foo'
+        self.drawBox('box.txt')
 
     def CreateMenuBar(self):
         "Create menu bar with Open, Exit"
@@ -99,26 +125,24 @@ class Frame(wx.Frame):
         else:
             print "Uncategorized: %s" %command
 
+    def moveBox(self, x, y, dx, dy):
+        self.panel.box_x = x
+        self.panel.box_y = y
+        self.panel.box_dx = dx
+        self.panel.box_dy = dy
+
     def drawBox(self, filename):
         csv_handle = csv.reader(open(filename, "rb"))
         box_info = []
         for data in csv_handle:
-          box_info.append(data)
-        self.setRectAttributes(box_info[0], box_info[1], box_info[2], box_info[3])
-        self.show_box = True
+          box_info = data
+        self.panel.setRectAttributes(int(box_info[0]), int(box_info[1]), int(box_info[2]), int(box_info[3]))
+        self.panel.show_box = True
+        self.panel.Update()
+        self.Update()
+        self.panel.Update()
+        self.Refresh()
 
-    def setRectAttributes(self, x, y, dx, dy):
-        self.box_x = x
-        self.box_y = y
-        self.box_dx = dx
-        self.box_dy = dy
-
-    def OnPaint(self, event):
-        dc = wx.ClientDC(self)
-        dc.SetPen(wx.Pen('#4c4c4c', 1, wx.TRANSPARENT))
-        if self.show_box and self.current_file:
-            dc.DrawRectangle(self.box_x, self.box_y, self.box_dx, self.box_dy)
-    
     def OnOpen(self, event):
         "Open image file, set title if successful"
         # Create file-open dialog in current directory
